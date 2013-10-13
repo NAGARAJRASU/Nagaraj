47339e71bc6804c61f38c5a672c80a9d796e6f87
Elcot <elcot@boss>
2013-10-13 15:05:27 +0530

    dskjdksjkd

---
+++ man.c
@@ -0,0 +1,29 @@
+.,m,.m,.m,.m.m.m.,m.,m.,m.,.m    XX
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# Committer: Elcot <elcot@boss>
+#
+# On branch master
+#
+# Initial commit
+#
+# Changes to be committed:
+#   (use "git rm --cached <file>..." to unstage)
+#
+#	new file:   man.c
+#	new file:   nag1.c
+#	new file:   nag2.c
+#	new file:   nag3.c
+#	new file:   nag4.c
+#	new file:   nag5.c
+#	new file:   nag6.c
+#	new file:   nag7.c
+#	new file:   nag8.c
+#	new file:   naga.c
+#
+
+XXCCCCCCCCCCCCCccc
+
+
+
